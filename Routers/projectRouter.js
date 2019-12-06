a6b9babf2c3e8196c10febfb01f5334193c19860
ignaciosm <ignaciosm@gmail.com>
2019-12-06 12:02:18 -0500

    get get by id and post work

--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -1,37 +1,49 @@
 const express = require('express');
 
-// const Projects = require('../projects/userDb');
+const Projects = require('../data/helpers/projectModel');
 const router = express.Router();
 router.use(express.json());
 
 // TEST
+// router.get('/', (req, res) => {
+//   res.send(`<h2>hello from projects!</h2>`);
+// });
+
+// GET projects
 router.get('/', (req, res) => {
-  res.send(`<h2>hello from projects!</h2>`);
+  Projects.get(req.all)
+  .then(projects => {
+    res.status(200).json(projects);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The posts information could not be retrieved." });
+  });
 });
 
-// READ projects
-// router.get('/', (req, res) => {
-//   Projects.get(req)
-//   .then(projects => {
-//     res.status(200).json(projects);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//     res.status(500).json({ error: "The posts information could not be retrieved." });
-//   });
-// });
+// GET projects/:id
+router.get('/:id', (req, res) => {
+  Projects.get(req.params.id)
+  .then(projects => {
+    res.status(200).json(projects);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The posts information could not be retrieved." });
+  });
+});
 
 // CREATE projects
-// router.post('/', validateUser, (req, res) => {
-//   Projects.insert(req.body)
-//   .then(user => {
-//     res.status(200).json(user);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//     res.status(500).json({ error: "The posts information could not be retrieved." });
-//   });
-// });
+router.post('/', (req, res) => {
+  Projects.insert(req.body)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ error: "The posts information could not be retrieved." });
+  });
+});
 
 // UPDATE projects
 // router.put('/:id', validateUserId, (req, res) => {
@@ -43,7 +55,6 @@ router.get('/', (req, res) => {
 //     console.log(error);
 //     res.status(500).json({ error: "The posts information could not be retrieved." });
 //   });
-
 // });
 
 // DELETE projects
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const projectRouter = require('./Routers/projectRouter');
 const actionRouter = require('./Routers/actionRouter');
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Works!</h2>`);
 });
 
 // ROUTES
@@ -15,22 +15,3 @@ server.use('/projects', projectRouter);
 server.use('/actions', actionRouter);
 
 module.exports = server;
-
-// const express = require('express');
-
-// const postsRouter = require('../posts/posts-router');
-
-// const server = express();
-
-// server.use(express.json());
-
-// server.get('/', (req, res) => {
-//   res.send(`
-//     <h2>Lambda Posts API</h>
-//     <p>Welcome to the Lambda Posts API</p>
-//   `);
-// });
-
-// server.use('/api/posts', postsRouter);
-
-// module.exports = server;
